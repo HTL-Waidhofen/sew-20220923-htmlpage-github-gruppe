568bf4e69665d8150e8aa06909dca07d1f111f71
Jan Ptacnik <jan.ptacnik@htlwy.at>
2022-09-23 15:51:34 +0200

    compatibility hotfix

--- navigationBar.js
+++ navigationBar.js
@@ -1,5 +1,14 @@
+let fr;
+function initialization(){
+
+}
 function displayGallery(){
-document.getElementById("p1").innerHTML=FileReader.readAsText("gallery.html");
+let fr=new FileReader();
+fr.onload=function(){
+                document.getElementById('output')
+                        .textContent=fr.result;
+            }
+document.getElementById("p1").innerHTML=fr.readAsText("gallery.html");
 console.log("p1Debug");}
 function displayDesc(){
 document.getElementById("p2").innerHTML=FileReader.readAsText("desc.html");}
