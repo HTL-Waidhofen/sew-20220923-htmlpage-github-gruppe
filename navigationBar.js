ef1eefb342006ed5db0aba1bfac7141e1a84d35f
Jan Ptacnik <jan.ptacnik@htlwy.at>
2022-09-23 16:06:04 +0200

    emergency push

--- navigationBar.js
+++ navigationBar.js
@@ -1,14 +1,12 @@
-let fr;
-function initialization(){
-
-}
-function displayGallery(){
+function JanRead(string fileName){
 let fr=new FileReader();
 fr.onload=function(){
-                document.getElementById('output')
-                        .textContent=fr.result;
-            }
-document.getElementById("p1").innerHTML=fr.readAsText("gallery.html");
+document.getElementById('output')
+.textContent=fr.result;}
+return fr.readAsText(fileName);
+}
+function displayGallery(){ 
+document.getElementById("p1").innerHTML=JanRead("gallery.html");
 console.log("p1Debug");}
 function displayDesc(){
 document.getElementById("p2").innerHTML=FileReader.readAsText("desc.html");}
